feb3c50006b05f3b10616aa1973c2c33c8b0a2be
Artur <arturrodriguesmota@hotmail.com>
2023-08-04 17:57:29 -0300

    Adicionando socket io

--- index.js
+++ index.js
@@ -5,12 +5,30 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+
 server.listen(process.env.PORT || 3000);
 
 app.use('/public', express.static(__dirname + '/public'))
 
 app.engine('.hbs', exphbs.engine({ extname: '.hbs', defaultLayout: "main"}));
 app.set('view engine', 'hbs');
+
+const historico = []
+
+// Io
+io.on('connection',(socket) =>{
+    console.log('nova conexao')
+    historico.forEach(item =>{
+        socket.emit('mudarCorBg')
+    })
+    
+    socket.on('mudarCorBg', () =>{
+        io.emit('mudarCorBg')
+        historico.push('mudarCorBg')
+    })
+})
+
+
 
 app.get('/', (req, res) => {
     res.render('index.hbs')
