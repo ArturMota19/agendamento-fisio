de25d8d2260a57462c3178e45e8bededb9ed2796
Artur <arturrodriguesmota@hotmail.com>
2023-08-04 17:40:12 -0300

    Colocando front-end para funcionar

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const exphbs  = require('express-handlebars');
 const app = express();
 
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 server.listen(process.env.PORT || 3000);
+
+app.use('/public', express.static(__dirname + '/public'))
+
+app.engine('.hbs', exphbs.engine({ extname: '.hbs', defaultLayout: "main"}));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('index.hbs')
+})
 
 console.log('Rodando na porta 3000');
